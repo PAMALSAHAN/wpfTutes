289d8b92b3ddaf31f352958b6d7cc5e699c9a61e
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-08-31 18:49:55 +0530

    working

---
+++ Corona/models/CoronaVirusCountry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corona.models
+{
+    public class CoronaVirusCountry
+    {
+        public string CountryName { get; set; }
+        public int CaseCount { get; set; }
+        public string CountryURI { get; set; }
+    }
+}
